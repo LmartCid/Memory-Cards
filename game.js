411c93869e30ac7084b2c12d9d04cf639449de51
LmartCid <lmartcid@gmail.com>
2025-07-31 00:05:54 +0200

    review and refactor application code

--- game.js
+++ game.js
@@ -4,6 +4,8 @@ const CARD_REVERSE = "❓";
 const MILISECONDS_FLIPPBACK_DELAY = 1000; 
 const MILISECONDS_TO_VICTORY_SCENE = 1000; 
 const DEFAULT_CONTENT_CARD = [1, 2, 3, 4, 5, 6, 7, 8]; 
+const PLAY_AGAIN_SYMBOL = "🔁"; 
+const BACK_MENU_SYMBOL = "🏠"; 
 
 function generateGameBoard() {
     const board = document.createElement("div"); 
@@ -12,8 +14,8 @@ function generateGameBoard() {
     return board;    
 } 
 
-function generateDeckContent(contentCards) {  
-    const duplicatedContent = [...contentCards, ...contentCards]; 
+function generateDeckContent() {  
+    const duplicatedContent = [...DEFAULT_CONTENT_CARD, ...DEFAULT_CONTENT_CARD]; 
     duplicatedContent.sort(() => {
         return Math.random() - 0.5; 
     }); 
@@ -21,7 +23,9 @@ function generateDeckContent(contentCards) {
 }
 
 
-function generateCards(container) { 
+function generateCards() { 
+
+    const boardMenu = document.querySelector('.board-game')
 
     const cards = []; 
     for(let i = 0; i < NUMBER_OF_CARDS; i++) {  
@@ -29,7 +33,7 @@ function generateCards(container) {
         const card = document.createElement("div"); 
         card.classList.add("card-game"); 
         card.innerText = CARD_REVERSE; 
-        container.appendChild(card);  
+        boardMenu.appendChild(card);  
         cards.push(card);  
     } 
     return cards; 
@@ -37,26 +41,36 @@ function generateCards(container) {
 
 
 
-function playGame(cards, arrayContents, board) { 
-    const cardsSelected = [];  
+function playGame() {
+
+    const board = document.querySelector('.board-game')
+    const cards = generateCards();  
+
+    const deckContent = generateDeckContent();  
+    let cardsSelected = [];  
+
     cards.forEach((card, index) => { 
         card.addEventListener("click", () => {
+            
+            
             if(cardsSelected.length < 2 && card.innerText === CARD_REVERSE) { 
-                card.innerText = arrayContents[index]; 
+                card.innerText = deckContent[index]; 
                 cardsSelected.push(card);  
             } 
+
             if(cardsSelected.length === 2) { 
-                compareCards(cardsSelected[0], cardsSelected[1]); 
-                cardsSelected.length = 0;  
+                compareCards(cardsSelected); 
+                cardsSelected = [];  
             }
-            const cardsUp = checkBoardGame(cards, board);
+
+            const cardsUp = checkIfPlayersWin();
+
             if(cardsUp) { 
                 setTimeout(() => {
                     generateWinnerScreen(board); 
-                    winnerSceneButtons = getWinnerSceneButtons();
-                    const playAgainButton = winnerSceneButtons[0]; 
+                    const winnerSceneButtons = getWinnerSceneButtons();
                     const backMenuButtonn = winnerSceneButtons[1]; 
-                    pressPlayAgainOption(playAgainButton, board, DEFAULT_CONTENT_CARD); 
+                    pressPlayAgainOption(board); 
                     pressBackMenuOption(backMenuButtonn, DEFAULT_CONTENT_CARD);  
 
                 }, MILISECONDS_TO_VICTORY_SCENE)
@@ -66,7 +80,12 @@ function playGame(cards, arrayContents, board) {
     }); 
 }
 
-function compareCards(card1, card2) {  
+
+
+function compareCards(cards) { 
+
+    const [card1, card2] = cards
+
     if(card1.innerText === card2.innerText) {
         console.log("son iguales");  
     } 
@@ -80,25 +99,137 @@ function compareCards(card1, card2) {
     }
 }
 
-function checkBoardGame(cards) { 
-    const cardsUp = cards.filter((card)=> {
-        return card.innerText !== CARD_REVERSE; 
-    })
+function checkIfPlayersWin() { 
 
-    if(cardsUp.length === NUMBER_OF_CARDS) {
-       return true; 
-    } 
-    else {
-        return false; 
-    }
+    const cards = document.querySelectorAll(".card-game"); 
+    const arrayCards = Array.from(cards);
+    const flippedCards = arrayCards.filter(card => {
+        return card.innerText !== CARD_REVERSE; 
+    }) 
+    return flippedCards.length === NUMBER_OF_CARDS;    
+}
+
+
+
+function generateMainMenu() {  
+    // Pinta la pantalla
+
+    // Crear boardMenu
+    const boardMenu = document.createElement("div"); 
+    boardMenu.classList.add("main-menu"); 
+    
+    createStartMenuButton(boardMenu)
+
+    // const selectThemeMenuOption = document.createElement("button");  
+    // selectThemeMenuOption.classList.add("selector-main-option"); 
+    // selectThemeMenuOption.innerText = "Themes"; 
+    // boardMenu.appendChild(selectThemeMenuOption);  
+
+    document.body.appendChild(boardMenu);  
+} 
+
+
+function createStartMenuButton (boardMenu) {
+
+      // Crear StartMenu
+    const startMenuButton = document.createElement("button"); 
+    startMenuButton.classList.add("start-main-option"); 
+    startMenuButton.innerText = "Start"; 
+
+    startMenuButton.addEventListener("click", () => goesToGame(boardMenu))
+
+    boardMenu.appendChild(startMenuButton); 
 
 }
 
-      
-const boardMenu = generateMainMenu();  
-const menuButtons = getMainMenuButtons(); 
-const startMenuButton = menuButtons[0]; 
-const themesMenuButton = menuButtons[1]; 
+
+function goesToGame (boardMenu) {
+
+        boardMenu.innerHTML = "";
+        boardMenu.classList.remove("main-menu"); 
+        boardMenu.classList.add("board-game");   
+        playGame();  
+}
 
 
-pressStartOptionMainMenu(startMenuButton, boardMenu, DEFAULT_CONTENT_CARD); 
+
+
+
+
+// function getMainMenuButtons() {  
+//     const mainMenuButtons = []; 
+//     const startButton = document.body.querySelector(".start-main-option"); 
+//     const themeButton = document.body.querySelector(".selector-main-option"); 
+//     mainMenuButtons.push(startButton, themeButton);  
+//     return mainMenuButtons; 
+// } 
+
+
+
+function pressThemesOptionMenu(selectThemesButton, board) {  
+    selectThemesButton.addEventListener("click", () => {
+        
+    })
+    
+}
+ 
+
+function createButton (text, className) {
+    const button = document.createElement("button"); 
+    button.innerText = text;  
+    button.classList.add(className); 
+    return button
+}
+
+function generateWinnerScreen(board) {   
+    board.innerHTML = ""; 
+    board.classList.remove("board-game"); 
+    board.classList.add("winner-screen"); 
+    
+    const playAgainButton = createButton(`${PLAY_AGAIN_SYMBOL} Play again`, "play-again-option")
+    const backMenuButton = createButton(`${BACK_MENU_SYMBOL} Back to main Menu`, "back-menu-option")
+
+    board.appendChild(playAgainButton);  
+    board.appendChild(backMenuButton);
+    return board;       
+}
+
+function getWinnerSceneButtons() {
+    const winnerSceneButtons = []; 
+    const playAgainButton = document.body.querySelector(".play-again-option"); 
+    const backMenuButton = document.body.querySelector(".back-menu-option"); 
+    winnerSceneButtons.push(playAgainButton, backMenuButton);  
+    return winnerSceneButtons; 
+} 
+
+function pressPlayAgainOption(board) { 
+
+    const winnerSceneButtons = getWinnerSceneButtons();
+     
+        winnerSceneButtons[0].addEventListener("click", () => {
+            board.remove(); 
+            generateGameBoard(); 
+            playGame();  
+        })
+    
+}
+
+function pressBackMenuOption(backMenuButton) {
+     
+        backMenuButton.addEventListener("click", () => { 
+            const winnerBoardScene = document.body.querySelector(".winner-screen");
+            winnerBoardScene.remove(); 
+            generateMainMenu();  
+            // const boardMenu = generateMainMenu();  
+            // const  mainMenuButtons = getMainMenuButtons(); 
+            // const startMenuButton = mainMenuButtons[0]; 
+        })
+    
+}
+    
+generateMainMenu();  
+// const menuButtons = getMainMenuButtons(); 
+// const startMenuButton = menuButtons[0]; 
+// const themesMenuButton = menuButtons[1]; 
+
+ 
